8e67c6e524a0f09fa37310e805e3a8b2dac9e5fe
brachtco <brachtdanny@gmail.com>
2023-03-03 11:31:50 -0700

    Code public/add-post.js file

---
+++ public/add-post.js
@@ -0,0 +1,22 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title]').ariaValueMax.trim();
+    const post_text = document.querySelector('textarea[name="post-text]').ariaValueMax.trim();
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, post_text}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (response.ok)  {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to Post');
+    }
+};
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
