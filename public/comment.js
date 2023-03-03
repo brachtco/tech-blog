8b1bdc6ad23546a9c2f95bc0348dcedca0be80db
brachtco <brachtdanny@gmail.com>
2023-03-03 12:18:44 -0700

    Code public/comment.js file

---
+++ public/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ post_id, comment_text }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace();///what is replaced where is redirect?
+        } else {
+            alert('Failed to post comment')
+        }
+    }
+}
