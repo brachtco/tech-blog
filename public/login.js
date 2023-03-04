8f860acfba67c255747e16fbb47c648c22c4a6b0
brachtco <brachtdanny@gmail.com>
2023-03-04 11:49:27 -0700

    Code public/js/login and loguot files

---
+++ public/login.js
@@ -0,0 +1,47 @@
+const loginFormHandler = async (event) => {
+    eventPreventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email  && password) {
+        
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: 
+                { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.eventPreventDefault();
+
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signip').value.trim();
+    const password =document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
